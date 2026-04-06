01282e972b231e8dbba8605d6f9b8684ad15934b
mcm <joohyuna2@gmail.com>
2026-04-06 17:55:49 +0900

    핸들러 (onAdd, onChange, onUpdateCount) 완성

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,46 @@ function App() {
     const [name, setName] = useState(""); // input 관리용 state
     const [cart, setCart] = useState([]); // 장바구니 관리용 state
 
+    const onAdd = event => {
+        event.preventDefault(); // 중단시키기
+
+        if (name === "") return; // 한줄뿐이면 중괄호 생략
+        setCart([...cart, { name: name, quantity: 1 }]);
+        setName("");
+    };
+
+    const onChange = e => {
+        setName(e.target.value);
+    };
+
+    const onUpdateCount = (index, number) => {
+        // 1가지 기능을 하는 함수를 만드는 건데,
+        // '1' 을 더한는 함수가 아니라, '수량을 변경하는 ' 함수를 만드는것
+        // 얼마를 바꿀것인가는 함수를 싷행 하 때 매개 변수를 통해 제어함
+        // 비슷한 기능을 하는 것은 통합을 하자
+
+        // 계산은 위에서
+        const newCart = [...cart];
+        const nextCount = newCart[index].quantity + number;
+        if (nextCount > 0) {
+            // 저장은 if문에서
+            newCart[index].quantity = nextCount;
+            setCart(newCart); // state는 직접적으로 수장하면 안된다
+        }
+    }
     return (
         <div>
             <h2>🛒 Simple Shop</h2>
             <fieldset>
                 <legend>상품추가</legend>
-                <form>
-                    <input placeholder={'상품녕을 입력하세요'} />
+                <form
+                    // (evern) => {}
+                    onSubmit={onAdd}>
+                    <input
+                        placeholder={"상품녕을 입력하세요"}
+                        onChange={onChange}
+                        value={name} // 값을 지워주려면
+                    />
                     <button type={"submit"}>카트에 담기</button>
                 </form>
             </fieldset>
@@ -21,7 +54,7 @@ function App() {
                 단! 이 안에 들어가는 값은 "갱체"로 작성함
                 css: background-color => backgroundColor => 카멜 케이스로 작성
             */}
-            <table border={1} cellpadding={10} cellspacint={0} style={{ width: "100%" }}>
+            <table border={1} cellPadding={10} cellSpacing={0} style={{ width: "100%" }}>
                 <thead>
                     <tr style={{ backgroundColor: "#f2f2f2" }}>
                         <th>상품명</th>
@@ -30,14 +63,33 @@ function App() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    {cart.map((value, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{value.name}</td>
+                                <td>
+                                    <button onClick={() => onUpdateCount(index, -1)}>-</button>
+                                    {value.quantity}
+                                    <button onClick={() => onUpdateCount(index, 1)}>+</button>
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => {
+                                            setCart(
+                                                cart.filter((v, i) => {
+                                                    return i !== index;
+                                                }),
+                                            );
+                                        }}>
+                                        삭제
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
-            <h3>총품목: 0개/총 수량: 0개</h3>
+            <h3>총품목: {cart.length}개/총 수량: 0개</h3>
         </div>
     );
 }
